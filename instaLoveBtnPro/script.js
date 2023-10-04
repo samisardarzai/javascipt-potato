4ba427c96173c92f2b6b9ae4559ce338e4e351f1
Sami Sardarzai <cleversam07@gmail.com>
2023-10-04 06:53:26 -0700

    add all

--- instaLoveBtnPro/script.js
+++ instaLoveBtnPro/script.js
@@ -1,9 +1,27 @@
 var con = document.querySelector("#container");
 var love= document.querySelector("i");
+var sam = document.querySelector("button");
 
 con.addEventListener("dblclick",function () {
     
     love.style.transform= "translate(-50%,-50%) scale(1)"
+    love.style.opacity= 0.8;
+    setTimeout(function () {
+        love.style.opacity= 0;
+       },3000);
+    setTimeout(function () {
+     love.style.transform= "translate(-50%,-50%) scale(0)";
+    },3000);
+    
+})
+
+sam.addEventListener("dblclick",function () {
+    
+    love.style.transform= "translate(-50%,-50%) scale(1)"
+    love.style.opacity= 0.8;
+    setTimeout(function () {
+        love.style.opacity= 0;
+       },3000);
     setTimeout(function () {
      love.style.transform= "translate(-50%,-50%) scale(0)";
     },3000);
